ae24a47b4b9ac43e6fa6bff4a08e6794a1199144
Woo Gim <woo@chequer.io>
2020-03-18 13:02:55 +0900

    Convert test to Typescript

--- src/sqlFormatter.ts
+++ src/sqlFormatter.ts
@@ -3,8 +3,15 @@ import N1qlFormatter from './languages/N1qlFormatter';
 import PlSqlFormatter from './languages/PlSqlFormatter';
 import StandardSqlFormatter from './languages/StandardSqlFormatter';
 
+export enum Language {
+  DB2 = 'db2',
+  N1QL = 'n1ql',
+  PLSQL = 'pl/sql',
+  SQL = 'sql',
+}
+
 export interface IConfig {
-  language?: string;
+  language?: Language | string;
   indent?: string;
   params?: any;
 }
@@ -17,13 +24,13 @@ export default {
     const cfg: IConfig = _cfg || {};
 
     switch (cfg.language) {
-      case 'db2':
+      case Language.DB2:
         return new Db2Formatter(cfg).format(query);
-      case 'n1ql':
+      case Language.N1QL:
         return new N1qlFormatter(cfg).format(query);
-      case 'pl/sql':
+      case Language.PLSQL:
         return new PlSqlFormatter(cfg).format(query);
-      case 'sql':
+      case Language.SQL:
       case undefined:
         return new StandardSqlFormatter(cfg).format(query);
       default:
